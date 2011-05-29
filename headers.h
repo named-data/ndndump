21e1bd079e9e3be4b69420504e857f9f02d2153c
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2011-05-28 20:59:41 -0700

    add headers; intercepts udp packets

---
+++ headers.h
@@ -0,0 +1,58 @@
+#ifndef HEADERS_H
+#define HEADES_H
+
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define	ETHERMTU	1500
+
+/* The number of bytes in an ethernet (MAC) address. */
+#define	ETHER_ADDR_LEN		6
+
+/* Length of an Ethernet header */
+#define ETHER_HDRLEN		14
+
+/* Structure of an Ethernet header. */
+struct	ether_header {
+	u_int8_t	ether_dhost[ETHER_ADDR_LEN];
+	u_int8_t	ether_shost[ETHER_ADDR_LEN];
+	u_int16_t	ether_type;
+};
+
+#define	IPVERSION	4
+#define	IP_MAXPACKET	65535		/* maximum packet size */
+
+/* Structure of an internet header, naked of options. */
+struct ip {
+	u_int8_t	ip_vhl;		/* header length, version */
+#define IP_V(ip)	(((ip)->ip_vhl & 0xf0) >> 4)
+#define IP_HL(ip)	((ip)->ip_vhl & 0x0f)
+	u_int8_t	ip_tos;		/* type of service */
+	u_int16_t	ip_len;		/* total length */
+	u_int16_t	ip_id;		/* identification */
+	u_int16_t	ip_off;		/* fragment offset field */
+#define	IP_DF 0x4000			/* dont fragment flag */
+#define	IP_MF 0x2000			/* more fragments flag */
+#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
+	u_int8_t	ip_ttl;		/* time to live */
+	u_int8_t	ip_p;		/* protocol */
+	u_int16_t	ip_sum;		/* checksum */
+	struct	in_addr ip_src,ip_dst;	/* source and dest address */
+};
+
+/* Udp protocol header. */
+struct udphdr {
+	u_int16_t	uh_sport;		/* source port */
+	u_int16_t	uh_dport;		/* destination port */
+	u_int16_t	uh_ulen;		/* udp length */
+	u_int16_t	uh_sum;			/* udp checksum */
+};
+
+#endif
--- ndndump.c
+++ ndndump.c
@@ -1,9 +1,53 @@
-#include <pcap.h>
-#include <stdio.h>
+#include "headers.h"
+
+#define MAX_SNAPLEN 65535
+
+//void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+	printf("got packet\n");
+}
 
 int main(int argc, char *argv[])
 {
-	char *dev = argv[1];
+	char *dev, errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t *handle;						/* Session handle */
+	struct bpf_program fp;				/* Compiled filter expression */
+	char filter_exp[] = "udp";			
+	bpf_u_int32 mask;					/* Netmask of the sniffing device */
+	bpf_u_int32 net;					/* IP of the sniffing device */
+	struct pcap_pkthdr header;			/* Header that pcap gives us */
+	const u_char *packet;				/* Actual packet */
+
+	dev = pcap_lookupdev(errbuf);
+	if (NULL == dev) {
+		fprintf(stderr, "couldn't find default device %s\n", errbuf);
+		return 2;
+	}
 	printf("Device: %s\n", dev);
+
+	if (-1 == pcap_lookupnet(dev, &net, &mask, errbuf)) {
+		fprintf(stderr, "couldn't get netmask for device %s\n", dev);
+	}
+	
+	handle = pcap_open_live(dev, MAX_SNAPLEN, 0, 1000, errbuf);
+	if (NULL == handle) {
+		fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+		return 2;
+	}
+	if (-1 == pcap_compile(handle, &fp, filter_exp, 0, net)) {
+		fprintf(stderr, "couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return 2;
+	}
+	if (-1 == pcap_setfilter(handle, &fp)) {
+		fprintf(stderr, "couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return 2;
+	}
+
+	pcap_loop(handle, -1, got_packet, NULL);
+	
+	pcap_freecode(&fp);
+	pcap_close(handle);
+	
 	return 0;
 }
