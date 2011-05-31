62426d90145e2247b98453db4a6ddcbf575cdd12
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2011-05-30 18:10:56 -0700

    ready for alpha release

--- src/ndndump.c
+++ src/ndndump.c
@@ -7,18 +7,29 @@
 #include <ccn/coding.h>
 #include <ccn/uri.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <ctype.h>
 
 
 #define MAX_SNAPLEN 65535
 #define CCN_MIN_PACKET_SIZE 5
+#define PBUF_SIZE 200
 
+struct flags_t {
+	int verbose;
+	int succinct;
+	int udp;
+	int tcp;
+};
+
+struct flags_t flags = {0, 0, 1, 1};
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-int dissect_ccn(const char *payload, int size_payload);
+int dissect_ccn(const char *payload, int size_payload, char *pbuf);
 int dissect_ccn_interest(const unsigned char *ccnb, int ccnb_size);
 int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size);
 void print_intercept_time();
-
+void usage();
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
 	/* declare pointers to packet headers */
 	const struct ether_header *ether_hdr;
@@ -32,6 +43,8 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 	int size_tcp;
 	int size_payload;
 
+	char pbuf[PBUF_SIZE];
+
 	ether_hdr = (struct ether_header *) (packet);
 	ip_hdr = (struct ip *) (packet + ETHER_HDRLEN);
 	size_ip = IP_HL(ip_hdr) * 4;
@@ -40,17 +53,22 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 		return;
 	}
 
-	printf("From: %s\t\tTo:%s\n", inet_ntoa(ip_hdr->ip_src), inet_ntoa(ip_hdr->ip_dst));
-
+	int printed;
 	switch(ip_hdr->ip_p) {
 		case IPPROTO_UDP:
+			if (!flags.udp)
+				return;
 			udp_hdr = (struct udphdr *)(packet + ETHER_HDRLEN + size_ip);
 			size_udp = UDP_HEADER_LEN;
 			size_payload = ntohs(ip_hdr->ip_len) - (size_ip + size_udp);
 			payload = (const char *)(packet + ETHER_HDRLEN + size_ip + size_udp);
-			dissect_ccn(payload, size_payload);
+			printed = sprintf(pbuf, "From: %s, ", inet_ntoa(ip_hdr->ip_src));
+			sprintf(pbuf + printed, "To:%s, Tunnel Type: UDP\n",  inet_ntoa(ip_hdr->ip_dst));
+			dissect_ccn(payload, size_payload, pbuf);
 			break;
 		case IPPROTO_TCP:
+			if (!flags.tcp)
+				return;
 			tcp_hdr = (struct tcphdr *)(packet + ETHER_HDRLEN + size_ip);
 			size_tcp = TH_OFF(tcp_hdr) * 4;
 			if (size_tcp < 20) {
@@ -59,16 +77,17 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 			}
 			payload = (const char *)(packet + ETHER_HDRLEN + size_ip + size_tcp);
 			size_payload = ntohs(ip_hdr->ip_len) - (size_ip + size_tcp);
-			dissect_ccn(payload, size_payload);
+			printed = sprintf(pbuf, "From: %s, ", inet_ntoa(ip_hdr->ip_src));
+			sprintf(pbuf + printed, "To:%s, Tunnel Type: TCP\n",  inet_ntoa(ip_hdr->ip_dst));
+			dissect_ccn(payload, size_payload, pbuf);
 			break;
 		default:
 			return;
 	}
 
-
 }
 
-int dissect_ccn(const char *payload, int size_payload) {
+int dissect_ccn(const char *payload, int size_payload, char *pbuf) {
 	struct ccn_skeleton_decoder skel_decoder;
 	struct ccn_skeleton_decoder *sd;
 	struct ccn_charbuf *c;
@@ -110,10 +129,18 @@ int dissect_ccn(const char *payload, int size_payload) {
 	
 	switch (packet_type) {
 		case CCN_DTAG_ContentObject:
+			print_intercept_time();
+			if (!flags.succinct) {
+				printf("%s", pbuf);
+			}
 			if (0 > dissect_ccn_content(ccnb, sd->index))
 				return 0;
 			break;
 		case CCN_DTAG_Interest:
+			print_intercept_time();
+			if (!flags.succinct) {
+				printf("%s", pbuf);
+			}
 			if (0 > dissect_ccn_interest(ccnb, sd->index))
 				return 0;
 			break;
@@ -125,13 +152,12 @@ int dissect_ccn(const char *payload, int size_payload) {
 
 }
 int dissect_ccn_interest(const unsigned char *ccnb, int ccnb_size) {
-	printf("dissecting ccn interest");
 	struct ccn_parsed_interest interest;
 	struct ccn_parsed_interest *pi = &interest;
 	struct ccn_charbuf *c;
 	struct ccn_indexbuf *comps;
-	const unsigned char *comp;
-	size_t comp_size;
+	//const unsigned char *comp;
+	//size_t comp_size;
 	const unsigned char *blob;
 	size_t blob_size;
 	ssize_t len;
@@ -148,73 +174,82 @@ int dissect_ccn_interest(const unsigned char *ccnb, int ccnb_size) {
 	len = pi->offset[CCN_PI_E_Name] - pi->offset[CCN_PI_B_Name];
 	c = ccn_charbuf_create();
 	ccn_uri_append(c, ccnb, ccnb_size, 1);
-	printf("Interest Name is %s\n", ccn_charbuf_as_string(c));
+	printf("Packet Type: Interest, Name: %s, ", ccn_charbuf_as_string(c));
+	/*
 	for (i = 0; i < comps->n - 1; i++) {
 		res = ccn_name_comp_get(ccnb, comps, i, &comp, &comp_size);
-		/* TODO: do something */
+		// TODO: do something 
 	}
-
-	/* MinSuffixComponents */
-	len = pi->offset[CCN_PI_E_MinSuffixComponents] - pi->offset[CCN_PI_B_MinSuffixComponents];
-	if (len > 0) {
-		int min_sc = pi->min_suffix_comps;
-		/* TODO: do something */
-	}
-
-	/* MaxSuffixComponents */
-	len = pi->offset[CCN_PI_E_MaxSuffixComponents] - pi->offset[CCN_PI_B_MaxSuffixComponents];
-	if (len > 0) {
-		int max_sc = pi->max_suffix_comps;
-		/* TODO: do something */
-	}
-
-	/* PublisherPublicKeyDigest */
-	/* Exclude */
-
-	/* ChildSelector */
-	len = pi->offset[CCN_PI_E_ChildSelector] - pi->offset[CCN_PI_B_ChildSelector];
-	if (len > 0) {
-		int order = pi->orderpref;
-		printf("ChildSelector: ");
-		switch (order) {
-		case 0: printf("leftmost/least\n");
-				break;
-		case 1: printf("rightmost/greatest\n");
-				break;
-		default: printf("invalid\n");
-		}
-	}
-
-	/* AnswerOriginKind */
-	len = pi->offset[CCN_PI_E_AnswerOriginKind] - pi->offset[CCN_PI_B_AnswerOriginKind];
-	if (len > 0) {
-		int origin = pi->answerfrom;
-		/* TODO: do something */
-	}
-
-	/* Scope */
-	len = pi->offset[CCN_PI_E_Scope] - pi->offset[CCN_PI_B_Scope];
-	if (len > 0) {
-		int scope = pi->scope;
-		/* TODO: do something */
-	}
-
-	/* InterestLifeTime */
-	len = pi->offset[CCN_PI_E_InterestLifetime] - pi->offset[CCN_PI_B_InterestLifetime];
-	if (len > 0) {
-		ccn_ref_tagged_BLOB(CCN_DTAG_InterestLifetime, ccnb, pi->offset[CCN_PI_B_InterestLifetime], pi->offset[CCN_PI_E_InterestLifetime], &blob, &blob_size);
-		lifetime = 0.0;
-		for (i = 0; i < blob_size; i++)
-			lifetime = lifetime *256.0 + (double)blob[i];
-		lifetime /= 4096.0;
-		/* TODO: do something */
-	}
+	*/
 
 	/* Nonce */
 	len = pi->offset[CCN_PI_E_Nonce] - pi->offset[CCN_PI_B_Nonce];
 	if (len > 0) {
 		ccn_ref_tagged_BLOB(CCN_DTAG_Nonce, ccnb, pi->offset[CCN_PI_B_Nonce], pi->offset[CCN_PI_E_Nonce], &blob, &blob_size);
-		/* TODO: do something */
+		printf("<");
+		for (i = 0; i < blob_size; i++) {
+			printf("%02x", *blob);
+			blob++;
+		}
+		printf(">\n");
+	}
+
+	if (flags.verbose) {
+		/* MinSuffixComponents */
+		len = pi->offset[CCN_PI_E_MinSuffixComponents] - pi->offset[CCN_PI_B_MinSuffixComponents];
+		if (len > 0) {
+			int min_sc = pi->min_suffix_comps;
+			printf("MinSC: %d, ", min_sc);
+		}
+
+		/* MaxSuffixComponents */
+		len = pi->offset[CCN_PI_E_MaxSuffixComponents] - pi->offset[CCN_PI_B_MaxSuffixComponents];
+		if (len > 0) {
+			int max_sc = pi->max_suffix_comps;
+			printf("MaxSC: %d, ", max_sc);
+		}
+
+		/* PublisherPublicKeyDigest */
+		/* Exclude */
+
+		/* ChildSelector */
+		len = pi->offset[CCN_PI_E_ChildSelector] - pi->offset[CCN_PI_B_ChildSelector];
+		if (len > 0) {
+			int order = pi->orderpref;
+			printf("ChildSelector: ");
+			switch (order) {
+			case 0: printf("leftmost/least, ");
+					break;
+			case 1: printf("rightmost/greatest, ");
+					break;
+			default: printf("invalid, ");
+			}
+		}
+
+		/* AnswerOriginKind */
+		len = pi->offset[CCN_PI_E_AnswerOriginKind] - pi->offset[CCN_PI_B_AnswerOriginKind];
+		if (len > 0) {
+			int origin = pi->answerfrom;
+			printf("AnswerOriginKind: %d, ", origin);	
+		}
+
+		/* Scope */
+		len = pi->offset[CCN_PI_E_Scope] - pi->offset[CCN_PI_B_Scope];
+		if (len > 0) {
+			int scope = pi->scope;
+			printf("Scope: %d, ", scope);
+		}
+
+		/* InterestLifeTime */
+		len = pi->offset[CCN_PI_E_InterestLifetime] - pi->offset[CCN_PI_B_InterestLifetime];
+		if (len > 0) {
+			ccn_ref_tagged_BLOB(CCN_DTAG_InterestLifetime, ccnb, pi->offset[CCN_PI_B_InterestLifetime], pi->offset[CCN_PI_E_InterestLifetime], &blob, &blob_size);
+			lifetime = 0.0;
+			for (i = 0; i < blob_size; i++)
+				lifetime = lifetime *256.0 + (double)blob[i];
+			lifetime /= 4096.0;
+			printf("InterestLifetime: %f\n", lifetime);
+		}
 	}
 
 	return 1;
@@ -222,7 +257,6 @@ int dissect_ccn_interest(const unsigned char *ccnb, int ccnb_size) {
 }
 
 int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
-	printf("dissecting ccn content");
 	struct ccn_parsed_ContentObject co;
 	struct ccn_parsed_ContentObject *pco = &co;
 	struct ccn_charbuf *c;
@@ -233,7 +267,6 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 	const unsigned char *blob;
 	int len;
 	int i;
-	double dt;
 	int res;
 
 	comps = ccn_indexbuf_create();
@@ -253,7 +286,13 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 	len = pco->offset[CCN_PCO_E_Name] - pco->offset[CCN_PCO_B_Name];
 	c = ccn_charbuf_create();
 	ccn_uri_append(c, ccnb, ccnb_size, 1);
-	printf("ContentObject name is %s\n", ccn_charbuf_as_string(c));
+
+	/* Content */
+	len = pco->offset[CCN_PCO_E_Content] - pco->offset[CCN_PCO_B_Content];
+	res = ccn_ref_tagged_BLOB(CCN_DTAG_Content, ccnb, pco->offset[CCN_PCO_B_Content], pco->offset[CCN_PCO_E_Content], &blob, &blob_size);
+	/* TODO: do something with content*/
+
+	printf("Packet Type: ContentObject, Name: %s, Content Size: %d\n", ccn_charbuf_as_string(c), len);
 
 	/* Name Components */
 	for (i = 0; i < comps->n - 1; i ++) {
@@ -266,60 +305,61 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 
     /* Timestamp */
 
-	/* Type */
-	len = pco->offset[CCN_PCO_E_Type] - pco->offset[CCN_PCO_B_Type];
-	if (len > 0) {
-		res = ccn_ref_tagged_BLOB(CCN_DTAG_Type, ccnb, pco->offset[CCN_PCO_B_Type], pco->offset[CCN_PCO_E_Type], &blob, &blob_size);
-		int type = pco->type;
-		printf("Content Type is: ");
-		switch (type) {
-		case CCN_CONTENT_DATA:  printf("Data\n");
-								break;
-		case CCN_CONTENT_ENCR:	printf("Encrypted\n");
-								break;
-		case CCN_CONTENT_GONE:  printf("Gone\n");
-								break;
-		case CCN_CONTENT_KEY:	printf("Key\n");
-								break;
-		case CCN_CONTENT_LINK:	printf("Link\n");
-								break;
-		case CCN_CONTENT_NACK:	printf("Nack\n");
-								break;
-		default:
-			break;
+	if (flags.verbose)  {
+		/* Type */
+		len = pco->offset[CCN_PCO_E_Type] - pco->offset[CCN_PCO_B_Type];
+		if (len > 0) {
+			res = ccn_ref_tagged_BLOB(CCN_DTAG_Type, ccnb, pco->offset[CCN_PCO_B_Type], pco->offset[CCN_PCO_E_Type], &blob, &blob_size);
+			int type = pco->type;
+			printf("Content Type: ");
+			switch (type) {
+			case CCN_CONTENT_DATA:  printf("Data, ");
+									break;
+			case CCN_CONTENT_ENCR:	printf("Encrypted, ");
+									break;
+			case CCN_CONTENT_GONE:  printf("Gone, ");
+									break;
+			case CCN_CONTENT_KEY:	printf("Key, ");
+									break;
+			case CCN_CONTENT_LINK:	printf("Link, ");
+									break;
+			case CCN_CONTENT_NACK:	printf("Nack, ");
+									break;
+			default:
+				break;
+			}
+
+		}
+		/* FreshSeconds */
+		len = pco->offset[CCN_PCO_E_FreshnessSeconds] - pco->offset[CCN_PCO_B_FreshnessSeconds];
+		if (len > 0) {
+			res = ccn_ref_tagged_BLOB(CCN_DTAG_FreshnessSeconds, ccnb,
+									  pco->offset[CCN_PCO_B_FreshnessSeconds],
+									  pco->offset[CCN_PCO_E_FreshnessSeconds],
+									  &blob, &blob_size);
+			i = ccn_fetch_tagged_nonNegativeInteger(CCN_DTAG_FreshnessSeconds, ccnb,
+													pco->offset[CCN_PCO_B_FreshnessSeconds],
+													pco->offset[CCN_PCO_E_FreshnessSeconds]);
+			
+			printf("FressSeconds: %d, ", i);
 		}
 
+		/* FinalBlockID */
+		len = pco->offset[CCN_PCO_E_FinalBlockID] - pco->offset[CCN_PCO_B_FinalBlockID];
+		if (len > 0) {
+			res = ccn_ref_tagged_BLOB(CCN_DTAG_FinalBlockID, ccnb,
+									  pco->offset[CCN_PCO_B_FinalBlockID],
+									  pco->offset[CCN_PCO_E_FinalBlockID],
+									  &blob, &blob_size);
+			
+			/* TODO: do something */
+			if (res == 0) 
+				printf("FinalBlockID: Yes\n");
+			else
+				printf("FinalBlockID: No\n");
+		}
+		/* KeyLocator */
 	}
-	/* FreshSeconds */
-    len = pco->offset[CCN_PCO_E_FreshnessSeconds] - pco->offset[CCN_PCO_B_FreshnessSeconds];
-    if (len > 0) {
-        res = ccn_ref_tagged_BLOB(CCN_DTAG_FreshnessSeconds, ccnb,
-                                  pco->offset[CCN_PCO_B_FreshnessSeconds],
-                                  pco->offset[CCN_PCO_E_FreshnessSeconds],
-                                  &blob, &blob_size);
-        i = ccn_fetch_tagged_nonNegativeInteger(CCN_DTAG_FreshnessSeconds, ccnb,
-                                                pco->offset[CCN_PCO_B_FreshnessSeconds],
-                                                pco->offset[CCN_PCO_E_FreshnessSeconds]);
-        
-		/* TODO: do something */
-    }
-
-	/* FinalBlockID */
-    len = pco->offset[CCN_PCO_E_FinalBlockID] - pco->offset[CCN_PCO_B_FinalBlockID];
-    if (len > 0) {
-        res = ccn_ref_tagged_BLOB(CCN_DTAG_FinalBlockID, ccnb,
-                                  pco->offset[CCN_PCO_B_FinalBlockID],
-                                  pco->offset[CCN_PCO_E_FinalBlockID],
-                                  &blob, &blob_size);
-        
-		/* TODO: do something */
-    }
-	/* KeyLocator */
-
-	/* Content */
-	len = pco->offset[CCN_PCO_E_Content] - pco->offset[CCN_PCO_B_Content];
-	res = ccn_ref_tagged_BLOB(CCN_DTAG_Content, ccnb, pco->offset[CCN_PCO_B_Content], pco->offset[CCN_PCO_E_Content], &blob, &blob_size);
-	/* TODO: do something */
 
 	return (ccnb_size);
 
@@ -327,23 +367,89 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 
 int main(int argc, char *argv[])
 {
-	char *dev, errbuf[PCAP_ERRBUF_SIZE];
+	char *dev = NULL;
+	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t *handle;						/* Session handle */
 	struct bpf_program fp;				/* Compiled filter expression */
 	char filter_exp[] = "ip";			
 	bpf_u_int32 mask;					/* Netmask of the sniffing device */
 	bpf_u_int32 net;					/* IP of the sniffing device */
-	struct pcap_pkthdr header;			/* Header that pcap gives us */
-	const u_char *packet;				/* Actual packet */
 
-	if (argc > 0) {
-		dev = argv[1];
-	} else 
+	int vflag = -1;
+	int sflag = -1;
+	int uflag = -1;
+	int tflag = -1;
+	int nflag = -1;
+
+	int c;
+
+	while ((c = getopt(argc, argv, "vsuti:")) != -1) {
+		switch (c) {
+		case 'v':
+			vflag = 1;
+			break;
+		case 's':
+			sflag = 1;
+			break;
+		case 'u':
+			uflag = 1;	
+			break;
+		case 't':
+			tflag = 1;
+			break;
+		case 'n':
+			nflag = 1;
+			break;
+		case 'i':
+			dev = optarg;
+			break;
+		case 'h':
+			usage();
+		case '?':
+			if (optopt == 'i')
+				fprintf(stderr, "Option -i requires an argument.\n");
+			else if (isprint(optopt))
+				fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+			else
+				fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+
+			usage();
+			return 1;
+		default:
+			usage();
+			return 1;
+
+		}
+	}
+
+	if (nflag == -1) {
+		if (vflag == 1 && sflag == 1) {
+			fprintf(stderr, "Conflicting options -v and -s\n");
+			return 1;
+		}
+
+		if (1 == vflag)
+			flags.verbose = 1;
+		if (1 == sflag)
+			flags.succinct = 1;
+	}
+	if (1 == uflag && -1 == tflag) {
+		flags.udp = 1;
+		flags.tcp = 0;
+	}
+	if (-1 == uflag && 1 == tflag) {
+		flags.udp = 0;
+		flags.tcp = 1;
+	}
+	
+	if (NULL == dev)
 		dev = pcap_lookupdev(errbuf);
+
 	if (NULL == dev) {
 		fprintf(stderr, "couldn't find default device %s\n", errbuf);
 		return 2;
 	}
+
 	printf("Device: %s\n", dev);
 
 	if (-1 == pcap_lookupnet(dev, &net, &mask, errbuf)) {
@@ -378,5 +484,15 @@ void print_intercept_time() {
 	struct tm *tm;
 	gettimeofday(&tv, &tz);
 	tm = localtime(&tv.tv_sec);
-	printf(" %d: %02d: %02d %d ", tm->tm_hour, tm->tm_min, tm->tm_sec, tv.tv_usec);
+	printf("%d:%02d:%02d.%d, ", tm->tm_hour, tm->tm_min, tm->tm_sec, tv.tv_usec);
+}
+
+void usage() {
+	printf("usage: ndndump [-nstuv] [-i interface]\n");
+	printf("\t\t-i: specify interface\n");
+	printf("\t\t-n: normal mode (default)\n");
+	printf("\t\t-s: sinccinct mode, will only print minimal info about Interest or Content Object\n");
+	printf("\t\t-t: track only tcp tunnel\n");
+	printf("\t\t-u: track only udp tunnel\n");
+	printf("\t\t-v: verbose mode, will print detail info about Interest or Content Object\n");
 }
