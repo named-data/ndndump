f8c5f2d131aa9c3c3c3972af76f1bbb6a444d7a8
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2011-06-02 16:19:44 -0700

    same as last one

--- src/ndndump.c
+++ src/ndndump.c
@@ -609,7 +609,7 @@ void print_intercept_time(const struct pcap_pkthdr *header, char *tbuf) {
 		sprintf(tbuf, "%d.%06d, ", (int) header->ts.tv_sec, (int)header->ts.tv_usec);
 	} else {
 		tm = localtime(&(header->ts.tv_sec));
-		sprintf(tbuf, "%d:%02d:%02d.%06d, ", tm->tm_hour, tm->tm_min, tm->tm_sec, header->ts.tv_usec);
+		sprintf(tbuf, "%d:%02d:%02d.%06d, ", tm->tm_hour, tm->tm_min, tm->tm_sec, (int)header->ts.tv_usec);
 	}
 }
 
