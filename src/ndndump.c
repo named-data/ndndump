274c32a59e728eea58d00e155174b8582133c94e
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2011-06-01 20:50:21 -0700

    print binary data; flag for printing signature

--- src/ndndump.c
+++ src/ndndump.c
@@ -31,19 +31,22 @@
 
 struct flags_t {
 	int verbose;
+	int signature;
 	int succinct;
 	int unix;
 	int udp;
 	int tcp;
 };
 
-struct flags_t flags = {0, 0, 0, 1, 1};
+struct flags_t flags = {0, 0, 0, 0, 1, 1};
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
 int dissect_ccn(const char *payload, int size_payload, char *pbuf, char *tbuf);
 int dissect_ccn_interest(const unsigned char *ccnb, int ccnb_size);
 int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size);
 void print_intercept_time(const struct pcap_pkthdr *header, char *tbuf);
+void print_payload(const u_char *payload, int len);
+void print_hex_ascii_line(const u_char *payload, int len, int offset);
 void usage();
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
 	/* declare pointers to packet headers */
@@ -313,10 +316,7 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 	}
 
 	
-
-
-
-	if (flags.verbose)  {
+	if (flags.signature) {
 		/* Signature */
 		len = pco->offset[CCN_PCO_E_Signature] - pco->offset[CCN_PCO_B_Signature];
 		if (len > 0) {
@@ -330,11 +330,14 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 										  pco->offset[CCN_PCO_B_DigestAlgorithm],
 										  pco->offset[CCN_PCO_E_DigestAlgorithm],
 										  &blob, &blob_size);
-				printf("DigestAlogrithm: ");
+				printf("DigestAlogrithm: \n\t");
+				print_payload(blob, blob_size);
+				/*
 				for (i = 0; i < blob_size; i++) {
 					printf("%02x", *blob);
 					blob++;
 				}
+				*/
 				printf("\n\t");
 			}
 			/* Witness */
@@ -347,14 +350,21 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 										  pco->offset[CCN_PCO_E_SignatureBits],
 										  &blob, &blob_size);
 				
-				printf("SignatureBits: ");
+				printf("SignatureBits: \n\t");
+				print_payload(blob, blob_size);
+				/*
 				for (i = 0; i < blob_size; i++) {
 					printf("%02x", *blob);
 					blob++;
 				}
+				*/
 				printf("\n");
 			}
 		}
+	}
+
+	if (flags.verbose)  {
+
 		/* SignedInfo */
 		printf("Signed Info:\n\t");
 		/* PublisherPublicKeyDigest */
@@ -365,11 +375,14 @@ int dissect_ccn_content(const unsigned char *ccnb, int ccnb_size) {
 									  pco->offset[CCN_PCO_B_PublisherPublicKeyDigest],
 									  pco->offset[CCN_PCO_E_PublisherPublicKeyDigest],
 									  &blob, &blob_size);
-			printf("PublisherPublicKeyDigest: ");
+			printf("PublisherPublicKeyDigest: \n\t");
+			print_payload(blob, blob_size);
+			/*
 			for (i = 0; i < blob_size; i++) {
 				printf("%02x", *blob);
 				blob++;
 			}
+			*/
 			printf("\n\t");
 
 		}
@@ -462,11 +475,15 @@ int main(int argc, char *argv[])
 	int uflag = -1;
 	int tflag = -1;
 	int nflag = -1;
+	int gflag = -1;
 
 	int c;
 
-	while ((c = getopt(argc, argv, "vsuthni:")) != -1) {
+	while ((c = getopt(argc, argv, "gvsuthni:")) != -1) {
 		switch (c) {
+		case 'g':
+			gflag = 1;
+			break;
 		case 'v':
 			vflag = 1;
 			break;
@@ -505,6 +522,8 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	if (1 == gflag)
+		flags.signature = 1;
 	if (1 == nflag) 
 		flags.unix = 1;
 
@@ -582,4 +601,98 @@ void usage() {
 	printf("\t\t-t: track only tcp tunnel\n");
 	printf("\t\t-u: track only udp tunnel\n");
 	printf("\t\t-v: verbose mode, will print detail info about Interest or Content Object\n");
+}
+
+/*
+* print data in rows of 16 bytes: offset   hex   ascii
+*
+* 00000   47 45 54 20 2f 20 48 54  54 50 2f 31 2e 31 0d 0a   GET / HTTP/1.1..
+*/
+void
+print_hex_ascii_line(const u_char *payload, int len, int offset)
+{
+
+	int i;
+	int gap;
+	const u_char *ch;
+
+	/* offset */
+	printf("%05d   ", offset);
+						
+	/* hex */
+	ch = payload;
+	for(i = 0; i < len; i++) {
+		printf("%02x ", *ch);
+		ch++;
+		/* print extra space after 8th byte for visual aid */
+		if (i == 7)
+			printf(" ");
+	}
+	/* print space to handle line less than 8 bytes */
+	if (len < 8)
+		printf(" ");
+																															
+	/* fill hex gap with spaces if not full line */
+	if (len < 16) {
+		gap = 16 - len;
+		for (i = 0; i < gap; i++) {
+			printf("   ");
+		}
+	}
+	printf("   ");
+																																					
+	/* ascii (if printable) */
+	ch = payload;
+	for(i = 0; i < len; i++) {
+		if (isprint(*ch))
+			printf("%c", *ch);
+		else
+			printf(".");
+		ch++;
+	}
+	printf("\n");
+}
+
+/*
+ * print packet payload data (avoid printing binary data)
+ */
+void
+print_payload(const u_char *payload, int len)
+{
+
+	int len_rem = len;
+	int line_width = 16;			/* number of bytes per line */
+	int line_len;
+	int offset = 0;					/* zero-based offset counter */
+	const u_char *ch = payload;
+
+	if (len <= 0)
+		return;
+
+	/* data fits on one line */
+	if (len <= line_width) {
+		print_hex_ascii_line(ch, len, offset);
+		return;
+	}
+
+	/* data spans multiple lines */
+	for ( ;; ) {
+		/* compute current line length */
+		line_len = line_width % len_rem;
+		/* print line */
+		print_hex_ascii_line(ch, line_len, offset);
+		/* compute total remaining */
+		len_rem = len_rem - line_len;
+		/* shift pointer to remaining bytes to print */
+		ch = ch + line_len;
+		/* add offset */
+		offset = offset + line_width;
+		/* check if we have line width chars or less */
+		if (len_rem <= line_width) {
+			/* print last line and get out */
+			print_hex_ascii_line(ch, len_rem, offset);
+			break;
+		}
+	}
+
 }
